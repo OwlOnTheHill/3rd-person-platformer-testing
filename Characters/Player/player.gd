cb9bb9dc133369ae07045be2e907790b3911b78c
OwlOnTheHill <masonmarcotte772@gmail.com>
2026-01-29 02:12:53 -0500

    Removed Prints, Raycast Code, +
    Removed all old debug prints from player.gd and raycast gd and removed some useless code from raycast script. Then fixed a crash issue caused by cleaner script where the script player was faster than the camera variable linking that itd cause it to crash as it couldnt find camera for the direction = direction.rotated(Vector3.UP, camera.global_rotation.y) line. Just added if camera: beforehand.

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -8,14 +8,11 @@ const WALL_GRAVITY = Vector3(0, -5.5, 0)
 var jumped_on = false : set = set_jumped_on
 func set_jumped_on(value):
 	jumped_on = value
-	print("jumped_on set to: ", value)
 
 @onready var raycast = $MeshInstance3D/RayCast3D
 @export var camera: Camera3D
 @export var rotation_speed: float = 18.0
 
-var debug_timer = 0.0
-const DEBUG_INTERVAL = 0.5
 var last_collided_wall: Node3D = null
 var sprinting_before_jump = false
 var was_on_floor = false
@@ -24,25 +21,22 @@ func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	#Add the gravity
+	# Add the gravity
 	if not is_on_floor():
 		if is_on_wall_only() and raycast.is_colliding() and velocity.y < 0:
 			velocity += WALL_GRAVITY * delta
 		else:
 			velocity += get_gravity() * delta
 	
-	# 1. detect when we just walked off a ledge
+	# Coyote Time logic
 	if was_on_floor and not is_on_floor() and velocity.y <= 0:
 		$CoyoteTimer.start()
 	
-	# 2. Update the "was_on_floor" state for the next frame
 	was_on_floor = is_on_floor()
 	
-	# get input direction and handle movement/deceleration
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	#changes keys to be oriented to camera direction
 	var camera_basis = $SpringArmPivot.transform.basis
-	# Ensure the camera's forward vector is projected horizontally
+	
 	var forward_direction = camera_basis.z.normalized()
 	forward_direction.y = 0
 	forward_direction = forward_direction.normalized()
@@ -53,12 +47,9 @@ func _physics_process(delta: float) -> void:
 	
 	var direction = (forward_direction * input_dir.y + right_direction * input_dir.x).normalized()
 
-	#rotates character mesh to be oriented to movement direction
+	# Smooth Mesh Rotation
 	if input_dir != Vector2.ZERO:
-		# Calculate the target rotation (where we WANT to face)
 		var target_rotation = $SpringArmPivot.rotation.y - input_dir.angle() - deg_to_rad(90)
-		
-		# Smoothly rotate the mesh toward that target
 		$MeshInstance3D.rotation.y = lerp_angle(
 			$MeshInstance3D.rotation.y,
 			target_rotation,
@@ -78,28 +69,21 @@ func _physics_process(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	direction = direction.rotated(Vector3.UP, camera.global_rotation.y)
+	if camera:
+		direction = direction.rotated(Vector3.UP, camera.global_rotation.y)
 
 	move_and_slide()
 
-	debug_timer += delta
-	if debug_timer >= DEBUG_INTERVAL:
-		debug_timer = 0.0 
-		print("jumped_on: ", jumped_on)
-		print("raycast.prev_collision: ", raycast.prev_collision)
-		print("raycast.get_collider(): ", raycast.get_collider())
-
+	# Wall collision reset logic
 	if raycast.is_colliding():
 		var current_collided_wall = raycast.get_collider()
 		if current_collided_wall != last_collided_wall:
 			jumped_on = false
-			print("Collision changed, jumped_on set to false")
 			last_collided_wall = current_collided_wall
 	else:
 		last_collided_wall = null
 
-	print("After Collision Check: jumped_on:", jumped_on, "prev:", raycast.prev_collision, "cur:", raycast.get_collider(), "is_wall:", is_on_wall_only())
-
+	# Jump Logic
 	if not is_on_floor() and Input.is_action_just_pressed("jump") and not jumped_on and raycast.is_colliding():
 		velocity.y = JUMP_VELOCITY
 		jumped_on = true
@@ -108,7 +92,6 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		jumped_on = false
 		sprinting_before_jump = false
-		print("on floor, jumped_on set to false")
 
 	if (is_on_floor() or !$CoyoteTimer.is_stopped()) and Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_VELOCITY
--- Scripts/Systems/ray_cast_3d.gd
+++ Scripts/Systems/ray_cast_3d.gd
@@ -11,18 +11,4 @@ func _physics_process(_delta):
 	if is_colliding():
 		var cur_collision: Node3D = get_collider()
 		if cur_collision != prev_collision:
-			if prev_collision != null:
-				print("Collided object changed from: ", prev_collision, " to: ", cur_collision)
-				handle_collision_exit(prev_collision)
-			handle_collision_enter(cur_collision)
 			prev_collision = cur_collision
-
-func handle_collision_enter(collided_object: Node3D):
-	print("Entered collision with: ", collided_object)
-	if collided_object.has_method("on_raycast_enter"):
-		collided_object.on_raycast_enter()
-
-func handle_collision_exit(collided_object: Node3D):
-	print("Exited collision with: ", collided_object)
-	if collided_object.has_method("on_raycast_exit"):
-		collided_object.on_raycast_exit()
